2e4994d28d43f1bbd9ee9d4130dcf6131f4dcc14
Celso-Rodriguez <celso.19rod@gmail.com>
2023-09-10 20:07:25 -0500

    Registro Modalidad - FK Deporte

---
+++ src/main/java/com/empresa/controller/ModalidadController.java
@@ -0,0 +1,66 @@
+package com.empresa.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.empresa.entity.Deporte;
+import com.empresa.entity.Modalidad;
+import com.empresa.service.DeporteService;
+import com.empresa.service.ModalidadService;
+
+@Controller
+public class ModalidadController {
+
+	@Autowired
+	private DeporteService service;
+	
+	@Autowired
+	private ModalidadService modalidadservice;
+	
+	@GetMapping(value = "/verModalidad" )
+	public String verAlumno() {return "registraModalidad";}
+	
+	@ResponseBody
+	@GetMapping(value="/listaDeporte")
+	public List<Deporte> cargaDeporte(){
+		return service.listaDeporte();	
+	}
+	
+	@PostMapping("/registraModalidad")
+	@ResponseBody
+	public Map<?, ?> registra(Modalidad obj){
+		HashMap<String, String> map = new HashMap<String, String>();
+		Modalidad objSalida = modalidadservice.insertaModalidad(obj);
+		
+		if (objSalida == null) {
+			map.put("MENSAJE", "Error en el registro");
+		}else {
+			map.put("MENSAJE", "Registro exitoso");
+		}
+		return map;
+	}
+	
+	@GetMapping("/buscaPorNombreModalidad")
+	@ResponseBody
+	public String validaNombre(String nombre) {
+		List<Modalidad> lstModalidad = modalidadservice.buscaPorNombre(nombre);
+		if (CollectionUtils.isEmpty(lstModalidad)) {
+			return "{\"valid\" : true }";
+		} else {
+			return "{\"valid\" : false }";
+		}
+	}
+	}
+			
+	
+
+
+
